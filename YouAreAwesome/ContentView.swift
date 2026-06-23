559c781592b5ef05dda128b62529950fae43db54
Rob Guldi <Roberticus01@Gmail.com>
2026-06-23 07:12:48 -0400

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Robbie Guldi on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            Text("I am an app developer!")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+        }
+        .padding()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Robbie Guldi on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
